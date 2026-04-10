4bc864bf675281bbff1f823594f5a263af864438
RJJJ <87w87w@gmail.com>
2026-04-10 06:20:47 -0700

    Initial Commit

---
+++ TECM/TECMApp.swift
@@ -0,0 +1,17 @@
+//
+//  TECMApp.swift
+//  TECM
+//
+//  Created by RJ on 10/4/2026.
+//
+
+import SwiftUI
+
+@main
+struct TECMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
